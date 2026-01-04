88c72a89b9629287e237ab49b4637aab2a38ca00
mo20007a-netizen <mo20007a@gmail.com>
2026-01-04 21:22:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+  toggle.textContent =
+    document.body.classList.contains("light") ? "🌙" : "☀️";
+});
